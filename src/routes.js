2e76117445fb8b2da2bf3b18fd4d77928451e94e
cauefurui <cauefurui@gmail.com>
2021-07-29 15:51:43 -0300

    creating create contact

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,6 @@ const router = Router();
 router.get('/contacts', ContactsController.index);
 router.get('/contacts/:id', ContactsController.show);
 router.delete('/contacts/:id', ContactsController.delete);
+router.post('/contacts', ContactsController.store);
 
 module.exports = router;
