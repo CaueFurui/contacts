1757cc97603cd2358a3afb645d683be759de3643
cauefurui <cauefurui@gmail.com>
2021-07-29 18:17:07 -0300

    creating categories methods

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const ContactsController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
 
 const router = Router();
 
@@ -9,5 +10,8 @@ router.get('/contacts/:id', ContactsController.show);
 router.delete('/contacts/:id', ContactsController.delete);
 router.post('/contacts', ContactsController.store);
 router.put('/contacts/:id', ContactsController.update);
+
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
 
 module.exports = router;
